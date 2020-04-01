ff33e28c9bd92fac21658c1035f263779833bf9d
sayani.nandi <sayaninandi1994@gmail.com>
2020-04-01 23:36:15 +0530

    Implementation of Nth fib, S1ToS2, BalanceParanthesis

--- src/main/java/recursion/impl/StringS1ToS2UsingRemoveInsertReplaceImpl.java
+++ src/main/java/recursion/impl/StringS1ToS2UsingRemoveInsertReplaceImpl.java
@@ -3,8 +3,29 @@ package recursion.impl;
 import recursion.StringS1ToS2UsingRemoveInsertReplace;
 
 public class StringS1ToS2UsingRemoveInsertReplaceImpl implements StringS1ToS2UsingRemoveInsertReplace {
+    public int min(int x , int y , int z){
+        if(x<= y && x<=z)
+            return x;
+        if(y<= x && y<=z)
+            return y;
+        else
+            return z;
+    }
 
     public int findMinOperationToConvertS1ToS2UsingRemoveInsertReplace(String s1, String s2, int m, int n) {
-        return 0;
+        if(m == 0)
+            return n ;
+        if(n == 0 )
+            return m;
+        if(s1.charAt(m-1) == s2.charAt(n-1))
+        {
+            return findMinOperationToConvertS1ToS2UsingRemoveInsertReplace(s1,s2,m-1,n-1);
+        }
+
+            return 1+min(findMinOperationToConvertS1ToS2UsingRemoveInsertReplace(s1,s2,m , n-1),
+            findMinOperationToConvertS1ToS2UsingRemoveInsertReplace(s1,s2,m-1,n),
+            findMinOperationToConvertS1ToS2UsingRemoveInsertReplace(s1,s2,m-1,n-1));
+
+
     }
 }
