5fcee290e09142e63d264cfdaed42c7ce5f8da1c
sayani.nandi <sayaninandi1994@gmail.com>
2020-04-01 23:07:57 +0530

    added till Paranthesis

--- src/main/java/recursion/impl/NthFibonacciNumberImpl.java
+++ src/main/java/recursion/impl/NthFibonacciNumberImpl.java
@@ -5,6 +5,12 @@ import recursion.NthFibonacciNumber;
 public class NthFibonacciNumberImpl implements NthFibonacciNumber {
 
     public int findNthFibonacciNumber(int n) {
-        return 0;
+        if(n==0)
+            return 0 ;
+        if(n==1)
+            return 1;
+
+
+        return findNthFibonacciNumber(n-1)+findNthFibonacciNumber(n-2);
     }
 }
--- src/main/java/stack/impl/BalanceParanthesisImpl.java
+++ src/main/java/stack/impl/BalanceParanthesisImpl.java
@@ -2,8 +2,45 @@ package stack.impl;
 
 import stack.BalanceParanthesis;
 
+import java.util.Stack;
+
 public class BalanceParanthesisImpl implements BalanceParanthesis {
     public boolean checkBalanceParanthesis(String str) {
-        return false;
+        Stack<Character> stk = new Stack<Character>();
+        if(str.length()== 0 || str==null){
+            return false;
+        }
+        int n = str.length();
+        for(int i = 0 ; i<n ; i++){
+            if(str.charAt(i)==')'){
+                if(!stk.isEmpty() && stk.peek()=='(')
+                    stk.pop();
+                else {
+                    return false;
+                }
+            }
+           else if(str.charAt(i) ==']'){
+               if((!stk.isEmpty() && stk.peek()=='['))
+                   stk.pop();
+               else {
+                   return false;
+               }
+            }
+            else if(str.charAt(i) =='}'){
+                if((!stk.isEmpty() && stk.peek()=='{'))
+                    stk.pop();
+                else {
+                    return false;
+                }
+            }
+            else{
+                stk.push(str.charAt(i));
+            }
+        }
+        if(stk.isEmpty())
+        return true;
+        else {
+            return false;
+        }
     }
 }
